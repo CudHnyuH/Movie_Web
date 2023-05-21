7ed75c65bfa3e0b357600276e3e4e6f3c1255193
cudhnyuh <phamhuynhduc805@gmail.com>
2023-05-21 18:03:55 +0700

    Update JS

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+
+// Header Sticky
+const header = document.querySelector("[data-header]");
+
+window.addEventListener("scroll", function () {
+
+  window.scrollY >= 10 ? header.classList.add("active") : header.classList.remove("active");
+
+});
+
+
+// GO TOP 
+const goTopBtn = document.querySelector("[data-go-top]");
+
+window.addEventListener("scroll", function () {
+
+  window.scrollY >= 500 ? goTopBtn.classList.add("active") : goTopBtn.classList.remove("active");
+
+});
